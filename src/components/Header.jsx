7287fd66c43c4778e4d84f2ab0e27abb429459ad
Ewa <ewa.edun@gmail.com>
2025-01-17 14:54:59 +0100

    put OG header back

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,30 @@
-import { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  return (
+  return(
     <div>
-      <header>
-        <nav className="navbar">
-          <Link to="/about" className="title">Hack Buddy</Link>
-          
-          <button 
-            className="menu-toggle" 
-            onClick={() => setIsMobile(!isMobile)}
-          >
-            ☰
-          </button>
-
-          {/* Navigation Buttons */}
-          <ul className={`nav-buttons ${isMobile ? "active" : ""}`}>
-            <li>
-              <Link to="/hacktracker">Hackathon Tracker</Link>
-            </li>
-            <li>
-              <Link to="/excusegenerator">Excuse Generator</Link>
-            </li>
-            <li>
-              <Link to="/submissionreadychecklist">Submission Ready Checklist</Link>
-            </li>
-            <li>
-              <Link to="/whatsnext">Whats Next</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+        <header>
+          <nav className="navbar">
+          <Link to='/about' className="title">Hack Buddy</Link>
+            <ul className="nav-buttons">
+                <li>
+                  <Link to="/hacktracker">Hackathon Tracker</Link> 
+                </li>
+                <li>
+                  <Link to="/excusegenerator">Excuse Generator</Link>
+                </li>
+                <li>
+                  <Link to="/submissionreadychecklist">Submission Ready Checklist</Link>
+                </li>
+                <li>
+                  <Link to="/whatsnext">Whats Next</Link>
+                </li>
+           </ul>
+          </nav>
+       </header>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
