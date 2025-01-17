1977234083a44f9eef133cc7b131516bc4b86671
Ewa <ewa.edun@gmail.com>
2025-01-17 14:45:53 +0100

    header mobile phone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,44 @@
+import { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 function Header() {
-  return(
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
     <div>
-        <header>
-          <nav className="navbar">
-          <Link to='/about' className="title">Hack Buddy</Link>
-            <ul className="nav-buttons">
-                <li>
-                  <Link to="/hacktracker">Hackathon Tracker</Link> 
-                </li>
-                <li>
-                  <Link to="/excusegenerator">Excuse Generator</Link>
-                </li>
-                <li>
-                  <Link to="/submissionreadychecklist">Submission Ready Checklist</Link>
-                </li>
-                <li>
-                  <Link to="/whatsnext">Whats Next</Link>
-                </li>
-           </ul>
-          </nav>
-       </header>
+      <header>
+        <nav className="navbar">
+          {/* Title */}
+          <Link to="/about" className="title">Hack Buddy</Link>
+          
+          {/* Toggle Button for Mobile */}
+          <button 
+            className="menu-toggle" 
+            onClick={() => setIsMobile(!isMobile)}
+          >
+            ☰
+          </button>
+
+          {/* Navigation Buttons */}
+          <ul className={`nav-buttons ${isMobile ? "active" : ""}`}>
+            <li>
+              <Link to="/hacktracker">Hackathon Tracker</Link>
+            </li>
+            <li>
+              <Link to="/excusegenerator">Excuse Generator</Link>
+            </li>
+            <li>
+              <Link to="/submissionreadychecklist">Submission Ready Checklist</Link>
+            </li>
+            <li>
+              <Link to="/whatsnext">Whats Next</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
